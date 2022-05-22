7d25811a088ed64a602451b6500dbb068a7791ba
= <=>
2022-05-22 19:57:30 +0200

    Disable CORS

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ Created on Sat May 14 15:34:49 2022
 """
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from api.api_v1.api import api_router
 from core.common.static_files import SPAStaticFiles
 
 app = FastAPI(title="Candid")
-
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_methods=['*'], allow_headers=['*'])
 app.include_router(api_router)
 app.mount('/', SPAStaticFiles(directory='frontend/pages', html=True), name='index')
